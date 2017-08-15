b13d89b2979a78ce5ea28cd6f3092bd2133ec221
lroemmele <lroemmele@gmail.com>
2017-08-14 20:34:55 -0400

    update to website

---
+++ scripts/couponScript.js
@@ -0,0 +1,21 @@
+﻿window.onload = function () {
+    var captchaArry = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    var arryLenght = 5;
+    var captchaGen = '';
+
+    for (var i = 0; i < arryLenght; i++) {
+        var rand = Math.floor(Math.random() * captchaArry.length);
+        console.log(rand);
+        captchaGen += captchaArry.substring(rand, rand+1)
+    }
+
+    document.getElementById('captchaGen').value = captchaGen;
+}
+
+function checkCaptcha() {
+    if (document.getElementById('humanVerify').value == document.getElementById('captchaGen').value) {
+        
+    } else {
+        alert("Please Enter Captcha");
+    }
+}
